f809b07cbd45113e2c70027460a37d7487319da9
Devang Rohilla <rohilladevang@email.com>
2026-08-29 16:18:44 +0530

    Functions created for data cleaning

---
+++ src/data_cleaning.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np 
+def clean_column_names(df):
+    # Converts the uppercase letter to lower and removes the blank spaces etc
+    df.columns = df.columns.str.lower().str.strip().str.replace(" ","_")
+    return df
+def clean_rating_column(df, col_name='city rating'):
+    """Converts string ratings to float and forces invalid entries ('--') to NaN."""
+    if col_name in df.columns:
+        df[col_name] = pd.to_numeric(df[col_name], errors='coerce')
+    return df
+def clean_rating_count(df,col_name='rating_count'):
+    #This function converts the rating_count from string to numeric
+    if col_name in df.columns:
+        # The r'(\d+)' regex extracts the first sequence of digits it finds in the string
+        df[col_name] = df[col_name].astype(str).str.extract(r'(\d+)').astype(float)
+    return df
